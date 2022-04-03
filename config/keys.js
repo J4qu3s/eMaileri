a987747866933b5f7ee02ef23cd2ce1f55a11968
J4qu3s <jarkko.hoyhtya@gmail.com>
2022-04-03 15:25:04 +0300

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -8,13 +8,3 @@ if(process.env.NODE_ENV === 'production') {
     // return dev.js
     module.exports = require('./dev');
 }
-
-/*
-clientID
-240907648720-9oo3por3ttq3dj9s21fvtbseebdn40s2.apps.googleusercontent.com
-ClientSecret
-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
-
-
-mongodb+srv://Kenguru:<password>@emailer-prod.12zsr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-*/
